c67aa266dc9d2dc269f123f0ab8a1da1f47b6936
Raquel Baptista Burger <raquelburgerpsi@gmail.com>
2022-06-28 20:44:19 -0300

    testando o chakra na tela cadastro

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../../theme'
+import { Box } from '@chakra-ui/react'
 
 export default class HomeScreen extends Component {
+  
   render() {
     return (
-      <div>HomeScreen</div>
+      <ChakraProvider theme={theme}>
+
+        <Box bg='blue.150'>HomeScreen</Box>
+        <Box bg='red.150'>HomeScreen</Box>
+        <Box bg='beige.150'>HomeScreen</Box>
+      </ChakraProvider>
     )
   }
 }
--- src/components/RegistrationScreen/RegistrationScreen.jsx
+++ src/components/RegistrationScreen/RegistrationScreen.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import axios from "axios"
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../../theme'
+import { Box } from "@chakra-ui/react"
+import { Flex, Spacer, Heading, ButtonGroup, Button, Input, Select, Text } from '@chakra-ui/react'
 
 export default class RegistrationScreen extends Component {
     state = {
@@ -12,7 +16,26 @@ export default class RegistrationScreen extends Component {
 
     render() {
         return (
-            <div>RegistrationScreen</div>
+            <ChakraProvider theme={theme}>
+                <Flex justifyContent='center'>
+                    <Box flexDirection="column" w='35rem' h='43rem' bg='blue.150'>
+                        <h1>Cadastre-se para ser um Ninja!</h1>
+                        <Text>Teste</Text>
+                        <Input mt='8%' mb='4%' placeholder='Título do anúncio'></Input>
+                        <Input mb='4%' placeholder='Descrição do serviço prestado'></Input>
+                        <Input mb='4%' type='number' placeholder='Informe o preço'></Input>
+                        <Select mb='4%' placeholder='Informe as formas de pagamento aceitas'>
+                            <option value='option1'>Pix</option>
+                            <option value='option2'>Dinheiro</option>
+                            <option value='option3'>Cartão de Crédito</option>
+                            <option value='option4'>Cartão de Débito</option>
+                        </Select>
+                        <Input type='date' placeholder='Informe o prazo disponível'></Input>
+                    </Box>
+                </Flex>
+
+
+            </ChakraProvider>
         )
     }
 }
