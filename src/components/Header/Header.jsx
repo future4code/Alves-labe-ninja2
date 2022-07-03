8ee4dd335a58da4f354fdb1b004811dc941cff92
David Henrique Souza da Silva <98848860+davidshenrique@users.noreply.github.com>
2022-07-03 13:27:02 -0300

    raposa head linkada ao home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Image, HStack, Text, Box, ButtonGroup, Heading, Center } from '@chakra-ui/react'
+import { Button, Flex, Link, Image, HStack, Text, Box, ButtonGroup, Heading, Center } from '@chakra-ui/react'
 import React, { Component } from 'react'
 import Logo from '../../assets/logo.png'
 import { FiShoppingCart } from 'react-icons/fi'
@@ -10,7 +10,8 @@ export default class Header extends Component {
 
     return (
       <HStack justifyContent='space-between'  bg='beige.200' mw='100%' h='15vh' alignItems='center' >
-        <Image  ml="0.5rem"src={Logo} alt='logo raposa' h='90%' />
+        <Link onClick={() => this.props.goToHome()}
+        ><Image ml="0.5rem"src={Logo} alt='logo raposa' w='6.5rem' h='6.5em' /></Link>
         <Center>
         <Heading pl='6rem' color="red.200">FOX Ninjas</Heading>
         </Center>
