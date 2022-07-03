f55157dd2ed825962bc417158f72e790cc83ad2a
David Henrique Souza da Silva <98848860+davidshenrique@users.noreply.github.com>
2022-07-03 06:52:55 -0300

    adicionado modal sobre nós

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -9,13 +9,49 @@ import {
     VStack,
     Divider,
     Icon,
+    Button
   } from "@chakra-ui/react";
   import { GrInstagram } from "react-icons/gr";
   import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
   import { FiTwitter } from "react-icons/fi";
+  import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+  } from '@chakra-ui/react'
+  import { useDisclosure } from '@chakra-ui/react';
 
 export default class Footer extends Component {
   render() {
+
+    const AboutUs = () => {
+        const { isOpen, onOpen, onClose } = useDisclosure()
+      
+        return (
+          <>
+            <Button textTransform="uppercase" color='beige.200'variant='link' onClick={onOpen}>Sobre nós</Button>
+      
+            <Modal onClose={onClose} isOpen={isOpen} isCentered>
+              <ModalOverlay />
+              <ModalContent  bg='beige.200'>
+                <ModalHeader>Sobre nós</ModalHeader>
+                <ModalBody>
+                  <Text>Somos uma startup de tecnologia com o objetivo de criar uma ponte entre profissionais qualificados com o cliente final. Centenas de profissionais já estão registrados em nossa plataforma e o cliente escolhe qual serviço deseja de forma prática e rápida.</Text>
+                </ModalBody>
+                <ModalFooter>
+                  <Button _hover={{bg:'red.200'}} color='beige.200' bg='blue.200' onClick={onClose}>Fechar</Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </>
+        )
+      }
+
+     
     
     return (
         <Box
@@ -52,7 +88,7 @@ export default class Footer extends Component {
             }}
           >
             <Flex justify="start" direction="column">
-              <Link textTransform="uppercase">Sobre nós</Link>
+              <AboutUs/>
             </Flex>
             <Flex justify="start" direction="column">
               <Link textTransform="uppercase">Contato</Link>
